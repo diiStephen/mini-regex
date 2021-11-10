efb2609e56312cf2c940400c9c0e78452898a4ce
Daniel <dstephenhono@gmail.com>
2021-11-09 21:00:50 -0500

    Refactoring.

--- syntax/src/syntax_tree/regex_syntax.rs
+++ syntax/src/syntax_tree/regex_syntax.rs
@@ -1,21 +1,21 @@
-type PtrExpression = Box<Expression>;
+type PtrExpr = Box<Expression>;
 
 #[derive(Debug, Clone)]
 pub enum Expression {
-    LITERAL(char),
-    STAR(PtrExpression),
-    UNION(PtrExpression, PtrExpression),
-    CONCAT(PtrExpression, PtrExpression),
+    CONST(char),
+    STAR(PtrExpr),
+    UNION(PtrExpr, PtrExpr),
+    CONCAT(PtrExpr, PtrExpr),
     EMPTY
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::syntax_tree::regex_syntax::Expression::{LITERAL, STAR, UNION};
+    use crate::syntax_tree::regex_syntax::Expression::{CONST, STAR, UNION};
 
     #[test]
     pub fn test_debug_expression() {
-        let u = LITERAL('a');
+        let u = CONST('a');
         let s = STAR(Box::new(u.clone()));
         let exp = UNION(Box::new(u), Box::new(s));
         print!("regex: {:?}", exp);
